33d6e4678dcceb5389562adfa60ad09254225b53
mvstone <tvv_mvs@mail.ru>
2021-10-04 17:15:21 +0300

    lesson6 оптимизация приложения

---
+++ MyVKclient/MyVKclient/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  MyVKclient
+//
+//  Created by Владислав Тихоненков on 12.06.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func loginButtom(_ sender: Any) {
+    }
+    
+}
+
